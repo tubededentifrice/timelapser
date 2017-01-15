10b2370dbf6d0a26a1b3583494955dd5827b24de
Vincent COURCELLE <Vincent COURCELLE>
2017-01-15 16:48:52 +0100

    Pictures

---
+++ TimelapserAndroidApp/app/src/main/java/com/courcelle/timelapser/timelapserandroidapp/MainActivity.java
@@ -0,0 +1,20 @@
+package com.courcelle.timelapser.timelapserandroidapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent cameraIntent = new Intent(this, HelloService.class);
+        //cameraIntent.putExtra("Front_Request", false);
+        //cameraIntent.putExtra("Quality_Mode", 0);
+        startService(cameraIntent);
+    }
+}
